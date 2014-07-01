479846e6d04518cbd4b39ca832622d2b7748b162
wilhempierre <wilhem@ymail.com>
2014-07-01 18:16:41 +0900

    Create Order History Pages

--- config/routes.rb
+++ config/routes.rb
@@ -5,9 +5,12 @@ Rails.application.routes.draw do
   resources :orders
   end
 
-  get 'pages/about'
-  get 'pages/contact'
+  get "pages/about"
+  get "pages/contact"
   get 'seller' => "listings#seller"
+  get 'sales' => "orders#sales"
+  get 'purchases' => "orders#purchases"
+
   root 'listings#index'
 
   # The priority is based upon order of creation: first created -> highest priority.
